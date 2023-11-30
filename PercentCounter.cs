f3be86546b8a4163c58698492cafece118cf75f0
Electrostats <Super_justy@hotmail.com>
2023-11-30 01:36:11 -0500

    Added Animate text option

--- PercentCounter.cs
+++ PercentCounter.cs
@@ -19,6 +19,11 @@ public class PercentCounter : MonoBehaviour
     private int[] scoreLeftover;
     private int[] scoreSums;
 
+    private float targetAcc;
+    private float currentAcc;
+    private float updateTimer;
+    private float timeSinceLastScore;
+
     public void Init(List<float[]> _leveldata, float _pb)
     {
         pb = _pb;
@@ -49,10 +54,31 @@ public class PercentCounter : MonoBehaviour
         foregroundText.text = 100.FormatDecimals() + "%";
         shadowText.text = 100.FormatDecimals() + "%";
 
+        foregroundText.supportRichText = true;
+        shadowText.supportRichText = true;
+
+        targetAcc = currentAcc = 100;
+        updateTimer = timeSinceLastScore = 0;
+
         RectTransform rect = GetComponent<RectTransform>();
         rect.anchoredPosition = new Vector2(rect.anchoredPosition.x - 50f, rect.anchoredPosition.y - 25f);
 
         scoreChanged += OnScoreChanged;
+    }
+
+    void Update()
+    {
+        if (Plugin.animateCounter.Value)
+        {
+            updateTimer += Time.deltaTime;
+            timeSinceLastScore += Time.deltaTime;
+            if (updateTimer > .02f && currentAcc != targetAcc)
+            {
+                currentAcc = EaseValue(currentAcc, targetAcc - currentAcc, timeSinceLastScore, .6f);
+                UpdateText(currentAcc);
+                updateTimer = 0;
+            }
+        }
     }
 
     void OnDestroy() => scoreChanged -= OnScoreChanged;
@@ -60,10 +86,24 @@ public class PercentCounter : MonoBehaviour
     internal void OnScoreChanged(int totalScore, int noteIndex)
     {
         float percent = GetPercent(totalScore, noteIndex);
+
+
+
+
+        if (Plugin.animateCounter.Value)
+        {
+            timeSinceLastScore = 0;
+            targetAcc = percent;
+        }
+        else
+            UpdateText(percent);
+    }
+
+    internal void UpdateText(float percent)
+    {
         string percentText = percent.FormatDecimals() + "%";
         foregroundText.text = percentText;
         shadowText.text = percentText;
-
         if (percent > pb)
         {
             foregroundText.color = Color.green;
@@ -85,4 +125,6 @@ public class PercentCounter : MonoBehaviour
             AccType.Decreasing => (float)(totalScore + scoreLeftover[noteIndex]) / maxScore * 100,
             _ => (float)totalScore / scoreSums[noteIndex] * 100,
         };
+    private float EaseValue(float current, float diff, float timeSum, float duration) =>
+            Mathf.Max(diff * (-Mathf.Pow(2f, -10f * timeSum / duration) + 1f) * 1024f / 1023f + current, 0f);
 }
